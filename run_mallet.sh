43cb50baaae4711fa402222e8ed4decc34f032b9
Justin Overfelt <joverfelt@gmail.com>
2013-11-03 17:45:13 -0500

    tuning some topic model params for some more fidelity in the model

--- run_mallet.sh
+++ run_mallet.sh
@@ -15,6 +15,6 @@ ruby tweet_archive_parser.rb $TWEETS_CSV
 
 $MALLET_EXECUTABLE import-dir --input tweets-data/ --output tweets-sequence.mallet --keep-sequence --remove-stopwords
 
-$MALLET_EXECUTABLE train-topics --input tweets-sequence.mallet --num-topics 7 --output-topic-keys tweets-keys --num-iterations 100 --num-top-words 15
+$MALLET_EXECUTABLE train-topics --input tweets-sequence.mallet --num-topics 25 --output-topic-keys tweets-keys --num-iterations 3000 --num-top-words 15
 
 cat tweets-keys
